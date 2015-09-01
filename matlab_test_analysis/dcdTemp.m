e19ae0cada25d558891ca438e744d51e5970427c
Matthew Kwiecien <mkwieci2@gmail.com>
2015-09-01 10:57:01 -0500

    useful scripts and functions for creating figures and checking calculations

---
+++ matlab_test_analysis/dcdTemp.m
@@ -0,0 +1,8 @@
+%Tom Script
+
+namdOutFile='/Users/nanotubes/Simulations/cnt200_4x4/PBC/300/100000/LongRunAug20_T_300K_F_0.01pN.dcd';
+[stat,tempData]=system(['awk "/^ENERGY:/ {printf \"%f\n\", \$13}" <' namdOutFile]);
+TNAMD=str2num(tempData);
+
+figure
+plot(TNAMD)
