0709cc725c11faa68f65bf61b0c43de2f0b92299
Matthew Kwiecien <mkwieci2@gmail.com>
2015-09-01 10:56:17 -0500

    Fixed an issue with indexing between python and matlab, now correctly calculates velocities and temperatures of waters and carbons

--- matlab_test_analysis/tempFind.m
+++ matlab_test_analysis/tempFind.m
@@ -1,9 +1,8 @@
-function [mean_vel, std_vel] = tempFind(fname,mode)
+function [mean_vel, std_vel, temps,stds] = tempFind(fin,nTot,temp,L,nRings,S,mode)
 % Parameters of Simulation
-% Total number of atoms
-nTot = 3813;
+fname = strcat('/Users/nanotubes/Simulations/cnt200_4x4/PBC/',num2str(temp),'/',num2str(L*1000),'/',fin);
+
 % Length of simulations
-length = 25;
 atomlist = 1:nTot;
 xyzlist = readdcd(fname,atomlist);
 
@@ -19,41 +18,45 @@ vx = vx.*(20.45482706); %1 A/ps
 vy = vy.*(20.45482706); %1 A/ps 
 vz = vz.*(20.45482706); %1 A/ps 
 
-nCarbon = 3216;
-nWater = 199;
+nWater = nRings+S;
+nCarbon = nTot - (nWater*3);
 
 % Two modes here: 0 is what is used for simply getting the mean and
 % standard deviation of the water molecules.  1 is used for bug checking
 % the simulation and ensuring that the kinetic energy and temperature is
 % correct.
+
 switch mode
     case 0
-        % Two different arrays here: one is taking the magnitude of the  
-        % velocity of the hydrogens and the oxygens and taking that total 
-        % velocity to be the velocity of the water.  However, rigidBonds is
-        % on in the simulation, so the hydrogen isn't really moving in the
-        % z-direction different than the water, so it just takes the
-        % velocity of the oxygen to be the velocity of the water. 
-        
-        % Using vWater2 for the reasons above.
-        
-        vWater = zeros(nWater,length);
-        vWater2 = zeros(nWater,length); 
-        for i = 1:length
-            vWater(:,i) = sqrt( vx(i,nCarbon+1:3:nTot).^2 + vy(i,nCarbon+1:3:nTot).^2 + vz(i,nCarbon+1:3:nTot).^2 );
-            vWater2(:,i) = vz(i,nCarbon+1:3:nTot);
-        end
-        
-        % (:,5:end) is put in here to wait until the simulation
-        % equilibrates to the simulation temperature.  This was just found
-        % by looking at different simulations, and could definitely be
-        % improved.
-        
-        mean_vel = mean(mean(vWater2,1));
-        std_vel = std(mean(vWater2,1));
-        
-        % Optional plotting if interested in seeing comparison between the
-        % total magnitude of the velocity and the regular velocity.
+    % Two different arrays here: one is taking the magnitude of the  
+    % velocity of the hydrogens and the oxygens and taking that total 
+    % velocity to be the velocity of the water.  However, rigidBonds is
+    % on in the simulation, so the hydrogen isn't really moving in the
+    % z-direction different than the water, so it just takes the
+    % velocity of the oxygen to be the velocity of the water. 
+
+    % Using vWater2 for the reasons above.
+
+    %vWater = zeros(nWater,simLength);
+    vWater2 = zeros(nWater,L); 
+    for i = 1:L
+        %vWater(:,i) = sqrt( vx(i,nCarbon+2:3:nTot).^2 + vy(i,nCarbon+2:3:nTot).^2 + vz(i,nCarbon+2:3:nTot).^2 );
+        % Velocity of all of the waters at 1 timestep of simulation
+        vWater2(:,i) = vz(i,nCarbon+2:3:nTot);
+    end
+
+    % (:,5:end) is put in here to wait until the simulation
+    % equilibrates to the simulation temperature.  This was just found
+    % by looking at different simulations, and could definitely be
+    % improved.
+
+    %First mean is the average velocity at each time step, second mean is
+    %the average velocity over the entire simulation.
+    mean_vel = (mean(mean(vWater2,1)));
+    std_vel = std(mean(vWater2,1));
+
+    % Optional plotting if interested in seeing comparison between the
+    % total magnitude of the velocity and the regular velocity.
 %         figure
 %         hold on
 %         set(gca, 'fontsize', 14)
@@ -62,52 +65,73 @@ switch mode
 %         plot(1:length, mean(vWater2,1), '--*b','linewidth',2)
 %         plot(1:length, mean(vx(:,nCarbon+1:3:nTot),2),'--r', 1:length, mean(vy(:,nCarbon+1:3:nTot),2),'--r', 1:length, mean(vz(:,nCarbon+1:3:nTot),2),'--k')
 %         legend('|v| water', 'vx water', 'vy water', 'vz water')
-        
+
+        temps = 0; stds = 0;
     case 1
-        
-        % Masses of different atoms
-        mC = 1.994e-26;
-        mH = 1.674e-27;
-        mO = 2.657e-26;
-        mCarbon = mC.*ones(1,nCarbon);
-        mOxygen = mO.*ones(1,nWater);
-        mHydrogen = mH.*ones(1,nWater);
-        % Boltzmann constant
-        k = 1.38065e-23; %boltzmann J/K
-        % Creating these different arrays to loop through each timestep of
-        % the simulation and find the kinetic energy at that point
-        KEcarbon = zeros(nCarbon,length);
-        KEoxy = zeros(nWater,length);
-        KEhydro1 = zeros(nWater,length);
-        KEhydro2 = zeros(nWater,length);
-        
-        % Using the fact that KE = 1/2 mv^2 to find KE of each atom.
-        for i = 1:length
-           KEcarbon(:,i) = 0.5 .* ( mCarbon .* sqrt( vx(i,1:nCarbon).^2 + vy(i,1:nCarbon).^2 + vz(i,1:nCarbon).^2).^2 );
-           KEoxy(:,i) = 0.5.*( mOxygen .* sqrt(vx(i,nCarbon+1:3:nTot).^2 + vy(i,nCarbon+1:3:nTot).^2 + vz(i,nCarbon+1:3:nTot).^2).^2 );
-           KEhydro1(:,i) = 0.5.*( mHydrogen .* sqrt(vx(i,nCarbon+2:3:nTot).^2 + vy(nCarbon+2:3:nTot).^2 + vz(nCarbon+2:3:nTot).^2).^2 );
-           KEhydro2(:,i) = 0.5.*( mHydrogen .* sqrt(vx(i,nCarbon+3:3:nTot).^2 + vy(i,nCarbon+3:3:nTot).^2 + vz(i,nCarbon+3:3:nTot).^2).^2 );
-        end
     
-        KEaveOxy = mean(KEoxy);
-        KEaveH1 = mean(KEhydro1);
-        KEaveH2 = mean(KEhydro2);
-        KEaveCarbon = mean(KEcarbon);
-        % Assuming 3 degrees of freedom, we have temperature calculated as
-        % below
-        Tcarbon = (2/3)*(1/k)* KEaveCarbon;
-        Toxy = (2/3)*(1/k)* KEaveOxy;
-        Th1 = (2/3)*(1/k)* KEaveH1;
-        Th2 = (2/3)*(1/k)* KEaveH2;
-        
-        figure
-        hold on
-        set(gca,'fontsize',14)
-        xlabel('Time (ps)')
-        ylabel('Temperature (K)')
-        title('T=1 K, L=10 ps, S = -1, 300 rings')
-        plot(1:length,Tcarbon,'--*r', 1:length,Toxy,'--*b', 1:length,Th1,'--*g', 1:length,Th2,'--*k')
-        legend('Carbon','Oxygen', 'Hydrogen 1','Hydrogen 2')
+    % Masses of different atoms
+    %mH = 1.674e-27;        
+    %mHydrogen = mH.*ones(1,nWater);
+    mO = 2.6567626e-26; % kg
+    mC = 1.99442e-26; %kg 
+    % Boltzmann constant
+    k = 1.38065e-23; %boltzmann J/K
+
+    % Using the fact that KE = 1/2 mv^2 to find KE of each atom.
+    for i = 1:L
+
+       KEO_x = (0.5*mO).*(vx(i,nCarbon+2:3:nTot).^2);
+       KEO_y = (0.5*mO).*(vy(i,nCarbon+2:3:nTot).^2);
+       KEO_z = (0.5*mO).*(vz(i,nCarbon+2:3:nTot).^2); % [KE] = kg m^2 s^-2
+
+       C_x = (0.5*mC).*(vx(i,1:nCarbon+1).^2);
+       C_y = (0.5*mC).*(vy(i,1:nCarbon+1).^2);
+       C_z = (0.5*mC).*(vz(i,1:nCarbon+1).^2);
+       
+    end
+
+    % From the fact that KE = 3/2 k T we have
+    %  T = 2 KE / k for each degree of freedom.
+    Tx = (2/k).*KEO_x;
+    Ty = (2/k).*KEO_y;
+    Tz = (2/k).*KEO_z;
+    
+    Cx = (2/k).*C_x;
+    Cy = (2/k).*C_y;
+    Cz = (2/k).*C_z;
+    
+    Txmean = mean(Tx);
+    Tymean = mean(Ty);
+    Tzmean = mean(Tz);
+    
+    Cxmean = mean(Cx);
+    Cymean = mean(Cy);
+    Czmean = mean(Cz);
+    
+    stdx = std(Tx);
+    stdy = std(Ty);
+    stdz = std(Tz);
+    
+    stdxc = std(Cx);
+    stdyc = std(Cy);
+    stdzc = std(Cz);
+    
+    temps = [Txmean,Tymean,Tzmean,Cxmean,Cymean,Czmean];
+    stds = [stdx,stdy,stdz,stdxc,stdyc,stdzc];
+    
+    % temporary to allow code to run
+    mean_vel = 0;
+    std_vel = 0;
+
+%     figure
+%     hold on
+%     set(gca,'fontsize',14)
+%     set(gca,'linewidth',2)
+%     xlabel('Time (ps)')
+%     ylabel('Temperature (K)')
+%     title('T_{therm} = 300K, F = 5.34pN')
+%     plot(1:199,Tx,'--*r', 1:199,Ty,'--*k', 1:199,Tz,'--*b')
+%     legend('T_{x}','T_{y}', 'T_{z}')
 
 end
 
